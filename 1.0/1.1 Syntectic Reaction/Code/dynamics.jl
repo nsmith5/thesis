f1b4033059683ad38e57f59d691579b9d9b4285c
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2016-09-20 17:11:19 -0400

    Horrible new algorithm, realllly bad oscillations and no spinodal (actually this is similar to last time

--- 1.0/1.1 Syntectic Reaction/Code/dynamics.jl
+++ 1.0/1.1 Syntectic Reaction/Code/dynamics.jl
@@ -1,16 +1,17 @@
 export step!
 
 function ΔFmix(c::Float64, s::State)
-	ϵ = -4.0 + s.ϵ₀*(s.σ - s.σ₀)
-	return s.ω*(c*log(2.0c) + (1-c)*log(2.0*(1-c)) + 0.5*ϵ*(c-0.5)^2)
+	@unpack ω, σ, σ₀, ϵ₀ = s
+	ϵ = -4.0 + ϵ₀*(σ - σ₀)
+	return ω * (c * log(2.0 * c) + (1 - c)*log(2.0 * (1. - c)) + 0.5 * ϵ * (c - 0.5) ^ 2)
 end
 
-δΔFmixδc(c::Float64, s::State) = s.ω*(log(2.0c) - log(2.0*(1-c)))
+δΔFmixδc(c::Float64, s::State) = s.ω*(log(2.0 * c) - log(2.0 * (1 - c)))
 
 function noise!(s::State)
-	N = s.N
-	c_scale = im*√(s.kbT*s.Mc/(s.Δx^2*s.Δt))
-	n_scale = im*√(s.kbT*s.Mₙ/(s.Δx^2*s.Δt))
+	@unpack N, kbT, Mc, Mₙ, Δx, Δt = s
+	c_scale = im*√(kbT*Mc/(Δx^2*Δt))
+	n_scale = im*√(kbT*Mₙ/(Δx^2*Δt))
 	for j in 1:N
 		for i in 1:N>>1 + 1
 			k = index_to_k(s, i, j)
@@ -21,26 +22,30 @@ function noise!(s::State)
 	return nothing
 end
 
+λ(c::Float64, s::State) = exp(-(c-0.5)^2/(2.0*s.αc^2))
+
 function set_nonlinear!(s::State)
-	third = 1/3
-	for i in 1:s.N*s.N
-		s.nₙₗ[i] = s.n[i]^2*(-0.5*s.η + third*s.χ*s.n[i]) + ΔFmix(s.c[i], s) - exp(-(s.c[i]-0.5)^2/(2s.αc^2))*s.C₂n[i]
-		s.cₙₗ[i] = (s.n[i] + 1.)*δΔFmixδc(s.c[i], s) - 0.5*s.n[i]*(-(s.c[i]-0.5)/s.αc^2*exp(-(s.c[i]-0.5)^2/(2s.αc^2))*s.C₂n[i])
+	@unpack N, n, c, η, χ, αc, C₂n = s
+	third = 1./3.
+	for i in 1:N*N
+		s.nₙₗ[i] = n[i]^2*(-0.5*η + third*χ*n[i]) + ΔFmix(c[i], s) - λ(c[i], s)*C₂n[i]
+		s.cₙₗ[i] = (1+n[i])*δΔFmixδc(c[i], s) + 0.5*n[i]*((c[i]-0.5)/αc^2*λ(c[i], s))*C₂n[i]
 	end
 end
 
 function calccorr!(s::State)
+	@unpack N, C₂n, kC₂n, C₂, ñ, fftplan = s
 	for i in 1:(s.N>>1+1)*s.N
-        s.kC₂n[i] = s.C₂[i]*s.ñ[i]
+        kC₂n[i] = C₂[i]*ñ[i]
     end
-    A_ldiv_B!(s.C₂n, s.fftplan, s.kC₂n)
+    A_ldiv_B!(C₂n, fftplan, kC₂n)
     return nothing
 end
 
 function step!(s::State)
-	@unpack ζ, Δt, ∇², Mₙ, Mc, ω, c̃, ñ, Wc, ξc, ξₙ = s
-	A_mul_B!(s.ñ, s.fftplan, s.n)
-	A_mul_B!(s.c̃, s.fftplan, s.c)
+	@unpack ζ, Δt, ∇², Mₙ, Mc, ω, c̃, ñ, n, c, Wc, ξc, ξₙ = s
+	A_mul_B!(ñ, s.fftplan, n)
+	A_mul_B!(c̃, s.fftplan, c)
 	calccorr!(s)
 	set_nonlinear!(s)
 	A_mul_B!(s.ñₙₗ, s.fftplan, s.nₙₗ)
