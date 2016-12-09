1381e64e63fbc081099b1ce87c9ef6215f99d6fd
Nathan Smith <smithn@physics.mcgill.ca>
2016-12-09 11:05:39 -0500

    Changed data folder to tools and started making a bash script for video production

---
+++ 1.0/1.- Nanocrystalline Gold/HPC Code/tools/h5topng.jl
@@ -0,0 +1,41 @@
+# h5topng.jl
+#
+# Usage:
+# 	julia h5topng.jl <filename>
+#
+# Convert an HDF5 Simulation record  <filename> into a mountain 
+# of png images
+
+using HDF5
+using PyPlot
+
+function make_pictures(filename)
+	println("Loading hdf5 datafile...")
+	fid = h5open(filename)
+
+	println("Writing figures...")
+	for time in names(fid)
+		group = fid[time]
+	
+		density	= group["Density"] |> read
+		concentration = group["Concentration"] |> read
+	
+		imshow(density, cmap="gray")
+		clim(-1, 1)		
+
+		imshow(concentration, cmap="coolwarm", alpha=0.5)	
+		clim(0, 1)
+		colorbar()	
+		
+		savefig(time*".png")
+		println("Wrote figure $time.png")		
+
+		clf()
+	end
+
+	close(fid)
+
+	return nothing
+end
+
+make_pictures(ARGS[1])
