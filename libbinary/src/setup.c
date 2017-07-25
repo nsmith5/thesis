ac21f8d4952e0b9ff51ac19a7f372e0fb243d3d2
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-07-25 00:03:23 -0700

    Document setup.c better

--- libbinary/src/setup.c
+++ libbinary/src/setup.c
@@ -1,3 +1,5 @@
+/*  Setup state and teardown state */
+
 #include <mpi.h>
 #include <hdf5.h>
 #include <fftw3.h>
@@ -7,12 +9,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include <stdbool.h>
+#include <signal.h>     // Catch signals for killing the simulation gracefully
+#include <stdbool.h>    // Booleans...just because they help reading
 
 #include "binary.h"
 
-#define WISDOM_FILE "fft_wisdom"
+#define WISDOM_FILE "fft_wisdom"    // Save fftw plans to this file
 
 double start_time = 0.0;
 
@@ -33,14 +35,17 @@ void init (int    argc,
     /* Import wisdom from file and broadcast */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    /* If the wisdom file exists, try to load it */
     if (rank == 0 && access (WISDOM_FILE, F_OK) != -1)
     {
         err = fftw_import_wisdom_from_filename (WISDOM_FILE);
         if (err == 0) my_error("Importing FFTW wisdom failed!");
     }
 
+    /* Broadcast wisdom if any exists */
     fftw_mpi_broadcast_wisdom (MPI_COMM_WORLD);
 
+    /* Wait for everyone */
     MPI_Barrier (MPI_COMM_WORLD);
     return;
 }
@@ -53,7 +58,7 @@ void finalize (void)
     fftw_mpi_gather_wisdom (MPI_COMM_WORLD);
 
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-    if (rank == 0)
+    if (rank == 0) // Only rank=0 proc will save the wisdom
     {
         err = fftw_export_wisdom_to_filename (WISDOM_FILE);
         if (err == 0)
@@ -63,9 +68,10 @@ void finalize (void)
         }
     }
 
+    /* Wait for everyone */
     MPI_Barrier (MPI_COMM_WORLD);
 
-    // Clean up threads, fftw and finalize MPI runtime
+    /* Clean up threads, fftw and finalize MPI runtime */
     fftw_mpi_cleanup ();
     MPI_Finalize ();
 
